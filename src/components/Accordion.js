f6a4370be6c89506b776176b4af64e77793fce0e
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-09 13:13:18 -0400

    Applied Semantic UI for Accordion styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { render } from '@testing-library/react'
 
 const Accordion = ({ items }) => {
-    return <h1>{items.length}</h1>
+
+    const renderedItems = items.map(item => {
+        return (
+            // use React.Fragment to remove the double border placed by Semantic UI
+            <React.Fragment key={item.title}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+
+    return (
+        <div className="ui styled accordion">
+            {renderedItems}
+        </div>
+    )
 }
 export default Accordion
