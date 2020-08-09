f3840a9300aefafd35eaec93c749c8668aaae182
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-09 13:42:01 -0400

    useState setup

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from '@testing-library/react'
 
 const Accordion = ({ items }) => {
 
-    const renderedItems = items.map(item => {
+    const [activeIndex, setActiveIndex] = useState(null)
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    }
+
+    const renderedItems = items.map((item, index) => {
         return (
             // use React.Fragment to remove the double border placed by Semantic UI
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div className="title active"
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
@@ -21,6 +29,7 @@ const Accordion = ({ items }) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     )
 }
