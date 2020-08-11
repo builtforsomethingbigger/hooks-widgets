00e04704be4aa38209b80c80069dd24ea1f735ed
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-10 21:40:43 -0400

    Dropdown update - color menu added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const Dropdown = () => {
-    return(
-        <div>
+const Dropdown = ({ options }) => {
 
+    const renderedOptions = options.map((option) => {
+        return (
+            <div key={option.value} className="item">
+                {option.label}
+            </div>
+        )
+    })
+
+    return(
+        <div className="ui form">
+            <div className="field">
+                <label className="label">SELECT A COLOR</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">Select Color</div>
+                    <div className="menu visible transition">
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
