0e4fa3bcd8b7a1141d703ab4f84df3cd6db38175
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-10 21:47:49 -0400

    Dropdown update - hide selected option from option list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,6 +3,12 @@ import React from 'react'
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const renderedOptions = options.map((option) => {
+
+        //if the color is selected, remove it as an option. only show the unselected options.
+        if (option.value === selected.value){
+            return null
+        }
+
         return (
             <div className="item" key={option.value} onClick={() => onSelectedChange(option)}>
                 {option.label}
