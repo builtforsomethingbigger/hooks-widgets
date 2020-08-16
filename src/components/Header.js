3dd2dce612f1e53f46db456cd6bd91c2710574f4
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-16 18:48:26 -0400

    Nav styling update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import Link from './Link'
 const Header = () => {
     return(
         <div className="ui secondary pointing menu">
-            <Link href="/" className="item">
+            <Link href="/" className="item onHover">
                 INFO MENU
             </Link>
-            <Link href="/search" className="item">
+            <Link href="/search" className="item onHover">
                 SEARCH
             </Link> 
-            <Link href="/dropdown" className="item">
+            <Link href="/dropdown" className="item onHover">
                 DROPDOWN
             </Link> 
-            <Link href="/translate" className="item">
+            <Link href="/translate" className="item onHover">
                 TRANSLATE
             </Link>     
         </div>
